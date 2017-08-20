729d08407c7890f5f302a4d5ee873e2095a42851
Pratik Kapasi <pratik.kapasi177@gmail.com>
2017-08-20 19:20:53 +0000

    Solves Naive Bayes quiz

--- naive_bayes/nb_author_id.py
+++ naive_bayes/nb_author_id.py
@@ -26,7 +26,15 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 #########################################################
 ### your code goes here ###
-
+from sklearn.naive_bayes import GaussianNB
+clf = GaussianNB()
+t0 = time()
+clf.fit(features_train, labels_train)
+print "Training time: ", time() - t0, "s"
+t1 = time()
+accuracy = clf.score(features_test, labels_test)
+print "Testing time: ", time() - t1, "s"
+print "Accuracy=",accuracy
 
 #########################################################
 
